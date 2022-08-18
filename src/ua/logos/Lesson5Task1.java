77ad469bd404bfb12f5e1b34a84c153375d8944f
SvitlanaManko <manko.svitlanka@gmail.com>
2022-08-18 23:35:41 +0300

    add task1 (array 100)

--- src/ua/logos/Lesson5Task1.java
+++ src/ua/logos/Lesson5Task1.java
@@ -6,10 +6,11 @@ public class Lesson5Task1 {
         for (int i = 0; i < hundred.length; i++) {
             hundred[i] = i + 1;
         }
-        for (int one : hundred) {
-            System.out.println(one + ", ");
+        for (int i = 0; i < hundred.length; i++) {
+            if (hundred[i] % 14 == 0) {
+                System.out.print(hundred[i] + ", ");
+            }
         }
-
     }
 }
 
