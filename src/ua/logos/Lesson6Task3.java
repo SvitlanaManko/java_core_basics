10a7d9010367a53142fdf71aa5a3ff21be539f4d
SvitlanaManko <manko.svitlanka@gmail.com>
2022-08-19 23:56:45 +0300

    add home task 3 (array Math.random)

---
+++ src/ua/logos/Lesson6Task3.java
@@ -0,0 +1,15 @@
+package ua.logos;
+
+public class Lesson6Task3 {
+    public static void main(String[] args) {
+        int[][] array = new int[5][7];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+
+                array[i][j] = (int) (Math.random() * 78) - 2;
+                System.out.print(array[i][j] + ", ");
+            }
+            System.out.println();
+        }
+    }
+}
