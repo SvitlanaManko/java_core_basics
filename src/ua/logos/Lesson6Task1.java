4fb67b884f6e1ff0e35ed2d72809cdbeafd51495
SvitlanaManko <manko.svitlanka@gmail.com>
2022-08-24 18:53:57 +0300

    add the annual compound interest formula

--- src/ua/logos/Lesson6Task1.java
+++ src/ua/logos/Lesson6Task1.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Lesson6Task1 {
     public static void main(String[] args) {
         double amountOfMoney;
-        int interestRate;
-        int numbersOfYears;
+        double interestRate;
+        double numbersOfYears;
         double depositAmount;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter amount of money($): ");
         amountOfMoney = scanner.nextDouble();
         System.out.print("Enter interest rate(%): ");
-        interestRate = scanner.nextInt();
+        interestRate = scanner.nextDouble();
         System.out.print("Enter numbers of years: ");
-        numbersOfYears = scanner.nextInt();
-        depositAmount = amountOfMoney + (amountOfMoney / 100 * interestRate * numbersOfYears);
+        numbersOfYears = scanner.nextDouble();
+        depositAmount = amountOfMoney * Math.pow((1+(interestRate/100)),numbersOfYears);
         System.out.print("The amount of your deposit for " + numbersOfYears + " years equal: " + depositAmount+"$");
     }
 }
