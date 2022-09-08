2b32559a519c69c245ced232226867e916fa6b3d
SvitlanaManko <manko.svitlanka@gmail.com>
2022-09-08 22:21:44 +0300

    add changes

--- src/ua/logos/task4/Methods.java
+++ src/ua/logos/task4/Methods.java
@@ -33,33 +33,31 @@ public class Methods {
             double number1 = EnterNumber.enterAnInteger();
             double number2 = EnterNumber.enterAnInteger();
             if (number1 < 0 && number2 < 0) {
-                System.out.println("IllegalArgumentException!");
                 throw new IllegalArgumentException();
             }
-            if ((number1 == 0 && number2 != 0) || (number1 != 0 && number2 == 0)) {
-                System.out.println("ArithmeticException!");
-                throw new ArithmeticException();
-            }
             if (number1 == 0 && number2 == 0) {
-                System.out.println("IllegalAccessException!");
                 throw new IllegalAccessException();
             }
-            //if (number1 > 0 && number2 > 0) {
-            //  System.out.println("MyException");
-            // throw new MyException();
-            // }
+            if ((number1 == 0 && number2 != 0) || (number1 != 0 && number2 == 0)) {
+                throw new ArithmeticException();
+            }
+
+           // if (number1 > 0 && number2 > 0) {
+            //    throw new MyException();
+            //}
             Methods methods = new Methods(number1, number2);
             System.out.println((int) number1 + "+" + (int) number2 + "=" + (int) methods.plus());
             System.out.println((int) number1 + "-" + (int) number2 + "=" + (int) methods.minus());
             System.out.println((int) number1 + "*" + (int) number2 + "=" + (int) methods.multiply());
             System.out.println((int) number1 + "/" + (int) number2 + "=" + methods.davide());
         } catch (IllegalArgumentException e) {
-
-        } catch (ArithmeticException e) {
-
+            System.out.println("IllegalArgumentException!");
         } catch (IllegalAccessException e) {
-
-            // } catch (MyException e) {
-        }
+            System.out.println("IllegalAccessException!");
+        } catch (ArithmeticException e) {
+            System.out.println("ArithmeticException!");
+        }// catch (MyException e) {
+        //    System.out.println("MyException");
+       // }
     }
 }
